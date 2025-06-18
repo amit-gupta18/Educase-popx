86d622509198628fc2743eed24637d1fef8e7c8e
Amit Kumar Gupta <amitgupta542173@gmail.com>
2025-06-18 19:43:32 +0530

    enhanced ui of landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 
 const Landing = () => {
 return (
-    <div>
             <div className='p-4 min-h-screen flex flex-col justify-between'>
                     <div>
                             <img src="/popx.png" alt="PopX Logo" className='w-60 h-60 rounded-full  mx-auto'/>
@@ -13,15 +12,14 @@ return (
                             <p className="mt-4 ">Lorem ipsum dolor sit amet <br /> consectetur adipisicing elit.</p>
                             <div className="mt-8 mb-8 flex flex-col gap-2 w-full">
                                     <Link to="/register">
-                                            <button className='bg-[#6c25ff] font-semibold text-white py-2 px-4 rounded-md w-full'>Create Account</button>
+                                            <button className='bg-[#6c25ff] font-semibold text-white py-4 px-4 rounded-md w-full'>Create Account</button>
                                     </Link>
                                     <Link to="/login">
-                                            <button className="bg-[#ceb9fa] font-semibold text-gray-900 py-2 px-4 rounded-md w-full">Already Registered? Login</button>
+                                            <button className="bg-[#ceb9fa] font-semibold text-gray-900 py-4 px-4 rounded-md w-full">Already Registered? Login</button>
                                     </Link>
                             </div>
                     </div>
-            </div>
-    </div>
+            </div>  
 )
 }
 
